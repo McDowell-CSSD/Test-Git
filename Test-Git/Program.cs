5aa0665273c5c656c5027d5338e7f3fb0b4b1d5d
McDowell <michael.mcdowell@cssd.ab.ca>
2017-08-24 07:34:41 -0600

    inital commit

---
+++ Test-Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGit
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
